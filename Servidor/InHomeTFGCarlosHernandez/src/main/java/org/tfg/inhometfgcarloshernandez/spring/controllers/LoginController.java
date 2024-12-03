e5395b1f768f36bd161b485006f8993669855677
Carlos Raul Hernández Campos <c.hcampos@outlook.es>
2024-12-03 13:19:44 +0100

    Update LoginController.java

--- Servidor/InHomeTFGCarlosHernandez/src/main/java/org/tfg/inhometfgcarloshernandez/spring/controllers/LoginController.java
+++ Servidor/InHomeTFGCarlosHernandez/src/main/java/org/tfg/inhometfgcarloshernandez/spring/controllers/LoginController.java
@@ -32,7 +32,6 @@ public class LoginController {
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
         Usuario usuario = this.usuarioService.findByEmail(loginRequest.getCorreo());
         LoginResponseDTO responseDTO = new LoginResponseDTO(usuario.getId());
-
         return ResponseEntity.ok(responseDTO);
     }
 
