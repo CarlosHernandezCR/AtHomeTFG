96842892e79260f1296195f7003810101aef40a4
Carlos Raul Hernández Campos <c.hcampos@outlook.es>
2025-02-24 00:33:11 +0100

    cambios varios

--- Cliente/AtHomeTFGAndroidCarlosHernandez/app/src/main/java/com/example/athometfgandroidcarloshernandez/ui/framework/screens/seleccionarcasa/SeleccionarCasaActivity.kt
+++ Cliente/AtHomeTFGAndroidCarlosHernandez/app/src/main/java/com/example/athometfgandroidcarloshernandez/ui/framework/screens/seleccionarcasa/SeleccionarCasaActivity.kt
@@ -1,10 +1,24 @@
 package com.example.athometfgandroidcarloshernandez.ui.framework.screens.seleccionarcasa
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -14,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -243,13 +258,16 @@ fun CrearCasaDialog(
     )
 }
 
+
 @Composable
 fun CasaItem(casa: CasaDetallesDTO, onCasaSelected: (Int) -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .clickable { onCasaSelected(casa.id) },
+            .clickable { onCasaSelected(casa.id) }
+            .border(2.dp, Color.Gray, RoundedCornerShape(8.dp))
+            .background(Color.LightGray, RoundedCornerShape(8.dp)),
         shape = MaterialTheme.shapes.medium
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
