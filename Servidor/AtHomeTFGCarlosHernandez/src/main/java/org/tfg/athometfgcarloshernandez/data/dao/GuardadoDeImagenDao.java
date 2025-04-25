254c5215d842811e4d056488e37f762dde5d3619
Carlos Raul Hernández Campos <c.hcampos@outlook.es>
2025-04-25 23:54:37 +0200

    arreglo de error de constructor

--- Servidor/AtHomeTFGCarlosHernandez/src/main/java/org/tfg/athometfgcarloshernandez/data/dao/GuardadoDeImagenDao.java
+++ Servidor/AtHomeTFGCarlosHernandez/src/main/java/org/tfg/athometfgcarloshernandez/data/dao/GuardadoDeImagenDao.java
@@ -1,8 +1,10 @@
 package org.tfg.athometfgcarloshernandez.data.dao;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.tfg.athometfgcarloshernandez.domain.errores.CustomedException;
@@ -10,7 +12,6 @@ import org.tfg.athometfgcarloshernandez.domain.errores.DatabaseException;
 import org.tfg.athometfgcarloshernandez.spring.common.constantes.ConstantesError;
 import org.tfg.athometfgcarloshernandez.spring.common.constantes.ConstantesSpring;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,11 +20,13 @@ import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class GuardadoDeImagenDao {
-    @Value(ConstantesSpring.IMAGE_STORE_PATH)
-    private String imageStorePath;
 
-    private final Path directorioUploads = Paths.get(imageStorePath);
+    private Path directorioUploads = null;
+    public GuardadoDeImagenDao(@Value(ConstantesSpring.IMAGE_STORE_PATH) String imageStorePath) {
+        this.directorioUploads = Paths.get(imageStorePath).toAbsolutePath().normalize();
+    }
 
     public String guardarImagen(MultipartFile imagen) {
         try {
